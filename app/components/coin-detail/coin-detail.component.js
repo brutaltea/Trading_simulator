0913634a3a81a10d760e74865f27d0b943426123
Unknown <teabrutal@gmail.com>
2017-12-19 04:52:41 +0200

    input field for price added, progress bar added, some styling

--- app/components/coin-detail/coin-detail.component.js
+++ app/components/coin-detail/coin-detail.component.js
@@ -5,6 +5,21 @@ angular.
     controller: ['$http', '$routeParams', '$interval',
       function CoinDetailController($http, $routeParams, $interval) {
         var self = this;
+        this.name = $routeParams.coinId;
+        this.progress = 0;
+
+        this.progressInterval = function() {
+          $interval(function(){
+            if (self.progress >= 100) {
+              self.progress = 0;
+              return self.progress;
+            } else {
+              self.progress = self.progress + 1.7
+              return self.progress;
+            }
+          }, 1000)
+        };
+        this.progressInterval();
 
         $http.get('https://min-api.cryptocompare.com/data/price?fsym=' + $routeParams.coinId + '&tsyms=USD').then(function(response) {
           self.coin = response.data;
@@ -19,10 +34,23 @@ angular.
        this.intervalFunction = function intervalFunction(){
          $interval(function(){
            self.getData();
-         }, 1000)
+           self.click();
+         }, 60000)
        };
-
        this.intervalFunction();
+       
+       this.click = function() {
+        $http.get('https://min-api.cryptocompare.com/data/price?fsym=' + $routeParams.coinId + '&tsyms=USD').then(function(response) {
+            self.coin = response.data;
+          });
+          if (!this.userPrice) {
+            return;
+          } else if (this.userPrice >= self.coin.USD){
+            console.log('bad')
+          } else {
+            console.log('good')
+          }
+       }
 
       }
     ]
